856c06f235aed397564d3d6f799dd8c21f94da56
penggrin12 <miner.sidor@gmail.com>
2026-08-16 19:17:44 +0300

    TALL_GRASS & add TYPE to voxels.def

--- src/voxeldata.hpp
+++ src/voxeldata.hpp
@@ -8,12 +8,18 @@
 #include "utils/utils.hpp"
 
 namespace Voxel {
+    enum Type {
+        FULL,
+        VEGETATION,
+        TYPE_MAX
+    };
+
     enum Id : uint8_t {
 #define VOXEL(name, id, ...) name = id,
 #include "../voxels.def"
 
 #undef VOXEL
-        MAX
+        ID_MAX
     };
 
     constexpr bool isSolid(const Id id) {
@@ -25,10 +31,15 @@ namespace Voxel {
     }
 }
 
-constexpr ivec2 VOXEL_ATLAS_OFFSETS[Voxel::MAX][6] = {
-#define VOXEL(_, __, ...) __VA_ARGS__,
+constexpr ivec2 VOXEL_ATLAS_OFFSETS[Voxel::Id::ID_MAX][6] = {
+#define VOXEL(_, __, ___, ...) __VA_ARGS__,
 #include "../voxels.def"
+#undef VOXEL
+};
 
+constexpr Voxel::Type VOXEL_TYPES[Voxel::Id::ID_MAX] = {
+#define VOXEL(_, __, type, ...) Voxel::Type::type,
+#include "../voxels.def"
 #undef VOXEL
 };
 
